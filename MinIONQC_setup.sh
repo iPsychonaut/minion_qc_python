0b4bfb16f5c09f79306b1e77d4af5cb7e6a468e5
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2024-04-14 11:55:04 -0600

    Update MinIONQC_setup.sh
    Updated to have better mamba install logic

--- MinIONQC_setup.sh
+++ MinIONQC_setup.sh
@@ -32,18 +32,20 @@ if ! command -v mamba &> /dev/null; then
         bash Mambaforge-Linux-x86_64.sh
         rm Mambaforge-Linux-x86_64.sh
     else
+        # If Mamba not installed
         echo "Please manually install Mambaforge for Windows."
+        echo -e "\e[33mFinal Instructions - Close this terminal; open a new one and run the following commands:\e[0m"
+        echo -e "\e[36m'mamba install -y -c bioconda -c conda-forge -c agbiome -c prkrekel numpy==1.26.3 pandas==2.2.0 matplotlib==3.8.2 seaborn==0.13.1 pyyaml==6.0.1 statsmodels==0.14.1\e[0m"
+        echo -e "\e[32mOnce the above are done Set-up is Complete!\e[0m"
     fi
+else
+    # Run Mamba installations
+    mamba install -y -c bioconda -c conda-forge -c agbiome -c prkrekel numpy==1.26.3 pandas==2.2.0 matplotlib==3.8.2 seaborn==0.13.1 pyyaml==6.0.1 statsmodels==0.14.1
 fi
 
 # Give Final Instructions
 echo -e "\e[32mMinIONQC.py  set-up done!\e[0m"
-echo -e "\e[33mFinal Instructions - Close this terminal; open a new one and run the following commands:\e[0m"
 
-# base installs
-echo -e "\e[36m'mamba install -y -c bioconda -c conda-forge -c agbiome -c prkrekel numpy==1.26.3 pandas==2.2.0 matplotlib==3.8.2 seaborn==0.13.1 pyyaml==6.0.1 statsmodels==0.14.1\e[0m"
-
-echo -e "\e[32mOnce the above are done Set-up is Complete!\e[0m"
 echo -e "\e[36mRun 'python MinIONQC.py -i /path/to/summary(s)' to execute the program!\e[0m"
 
 # About Section
